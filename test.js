de17448e92a44962fb6ce54e471a110f5ef2f7f3
Ralf S. Engelschall <rse@engelschall.com>
2016-11-16 23:29:18 +0100

    showcase custom structures based on JSON type

--- test.js
+++ test.js
@@ -15,14 +15,16 @@ let definition = `
     scalar Date
     scalar UUID
     scalar JSON
+    scalar Coord
     type RootQuery {
         exampleVoid: Void
         exampleMyInt(num: MyInt): Int
         exampleMyFloat(num: MyFloat): Float
         exampleMyString(str: MyString): String
-        exampleDate(date1: Date, date2: Date): Date
-        exampleUUID(uuid1: UUID, uuid2: UUID): UUID
-        exampleJSON(json1: JSON, json2: JSON): JSON
+        exampleDate(date: Date): Date
+        exampleUUID(uuid: UUID): UUID
+        exampleJSON(json: JSON): JSON
+        exampleCoord(coord: Coord): Coord
     }
 `
 let resolvers = {
@@ -33,6 +35,7 @@ let resolvers = {
     Date:     GraphQLToolsTypes.Date({ name: "MyDate" }),
     UUID:     GraphQLToolsTypes.UUID({ name: "MyUUID" }),
     JSON:     GraphQLToolsTypes.JSON({ name: "MyJSON" }),
+    Coord:    GraphQLToolsTypes.JSON({ name: "Coord", struct: "{ x: number, y: number }" }),
     RootQuery: {
         exampleVoid: (root, args, ctx, info) => {
             return {}
@@ -47,13 +50,16 @@ let resolvers = {
             return args.str
         },
         exampleDate: (root, args, ctx, info) => {
-            return args.date1
+            return args.date
         },
         exampleUUID: (root, args, ctx, info) => {
-            return args.uuid1
+            return args.uuid
         },
         exampleJSON: (root, args, ctx, info) => {
-            return args.json1
+            return args.json
+        },
+        exampleCoord: (root, args, ctx, info) => {
+            return args.coord
         }
     }
 }
@@ -62,20 +68,26 @@ let schema = GraphQLTools.makeExecutableSchema({
     resolvers: resolvers,
 })
 let query = `
-    query ($json: JSON, $uuid: UUID, $date: Date) {
+    query ($date: Date, $uuid: UUID, $json: JSON, $coord: Coord) {
         exampleVoid,
         exampleMyInt(num: 100),
         exampleMyFloat(num: 42.7),
         exampleMyString(str: "foo")
-        exampleDate(date1: $date, date2: "2016-08-16T00:01:02.000Z"),
-        exampleUUID(uuid1: $uuid, uuid2: "6cbe657c-63e3-11e6-aa83-080027e303e4"),
-        exampleJSON(json1: $json, json2: { foo: "bar", baz: 42, quux: true }),
+        date1: exampleDate(date: $date),
+        date2: exampleDate(date: "2016-08-16T00:01:02.000Z"),
+        uuid1: exampleUUID(uuid: $uuid),
+        uuid2: exampleUUID(uuid: "6cbe657c-63e3-11e6-aa83-080027e303e4"),
+        json1: exampleJSON(json: $json),
+        json2: exampleJSON(json: { foo: "bar", baz: 42, quux: true }),
+        coord1: exampleCoord(coord: $coord),
+        coord2: exampleCoord(coord: { x: 7, y: 42 })
     }
 `
 let variables = {
-    json: { foo: "bar", baz: 42, quux: true },
+    date: "2016-08-16T00:01:02.000Z",
     uuid: "6cbe657c-63e3-11e6-aa83-080027e303e4",
-    date: "2016-08-16T00:01:02.000Z"
+    json: { foo: "bar", baz: 42, quux: true },
+    coord: { x: 7, y: 42 }
 }
 GraphQL.graphql(schema, query, null, null, variables).then((result) => {
     console.log("OK", result)
